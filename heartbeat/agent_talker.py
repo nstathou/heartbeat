470bd4dcc60e7e123c99eb7bf7a6cc2255704719
nstathou <nstathou@gmail.com>
2023-09-22 14:53:50 +0200

    added argument for agent name

--- heartbeat/agent_talker.py
+++ heartbeat/agent_talker.py
@@ -1,44 +1,48 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
-
+import argparse
 
 class AgentTalker(Node):
     """ This is the agent talker class. It is responsible for broadcasting
         its state to the base station and all agents. """
 
-    def __init__(self):
+    def __init__(self, agent_name):
         """ Initialize the AgentTalker class. """
-        super(AgentTalker, self).__init__('agent_talker')
+        super(AgentTalker, self).__init__(agent_name + '_talker')
         ## Create a publisher.
-        self.publisher = self.create_publisher(String, 'agent/heartbeat', 10)
+        self.publisher = self.create_publisher(String, agent_name + '/heartbeat', 10)
         ## Create a timer.
         timer_period = 1.0
         self.timer = self.create_timer(timer_period, self.timer_callback)
         ## Create a counter.
         self.i = 0
+        self.agent_name = agent_name
 
     def timer_callback(self):
         """ Timer callback function. """
         msg = String()
-        msg.data = str(self.i)
+        msg.data = f"{self.agent_name} - {self.i}"
         self.publisher.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg.data)
         self.i += 1 
 
-
 def main(args=None):
     """ Main function. """
     rclpy.init(args=args)
-    ## Create an AgentTalker object.
-    agent_talker = AgentTalker()
+    
+    parser = argparse.ArgumentParser(description='AgentTalker Node')
+    parser.add_argument('agent_name', type=str, help='Name of the agent')
+    args = parser.parse_args()
+    
+    ## Create an AgentTalker object with the provided agent name.
+    agent_talker = AgentTalker(args.agent_name)
     ## Spin the AgentTalker object.
     rclpy.spin(agent_talker)
     ## Destroy the AgentTalker object.
     agent_talker.destroy_node()
     ## Shutdown the ROS2 client.
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
