888cc73c7779e522cd1e10bf3282fb7d8165c82c
nstathou <nstathou@gmail.com>
2023-09-22 16:31:32 +0200

    added base station status

--- heartbeat/bs_listener.py
+++ heartbeat/bs_listener.py
@@ -19,14 +19,36 @@ class BSListener(Node):
                 'agent' + str(i) + '/heartbeat',
                 lambda msg, i=i: self.listener_callback(msg, i),  # Using a lambda function to capture the value of i
                 10))
-
+        ## Create a publisher.
+        self.publisher = self.create_publisher(String, 'base_station/status', 10)
+        ## Create a timer.
+        timer_period = 1.0  # seconds
+        self.timer = self.create_timer(timer_period, self.status_identifier)
+        ## Create a list of messages.
         self.msgs = ['' for _ in range(NUM_AGENTS)]
 
     def listener_callback(self, msg, i):
         """ Listener callback function. """
-        self.get_logger().info('Base station heard: "%s"' % msg.data)
+        self.get_logger().info('Base station heard: "%s"' % msg.data + ' from agent' + str(i))
         ## Publish a message to the base_station/status topic.
         self.msgs[i] = msg.data
+
+    def status_identifier(self):
+        """ Status identifier function for the base station. For all agents that you receive 
+            a message identify as connected, otherwise as disconnected. Publish a single string
+            message to the base_station/status topic containing the status of all agents. """
+        status = ''
+        for i in range(NUM_AGENTS):
+            if len(self.msgs[i]) == 0:
+                status += 'agent' + str(i) + ': disconnected, '
+            else:
+                status += 'agent' + str(i) + ': connected, '
+        ## Publish a message to the base_station/status topic.
+        msg = String()
+        msg.data = status
+        self.publisher.publish(msg)
+        ## Reset the message.
+        self.msgs = ['' for _ in range(NUM_AGENTS)]
 
 def main(args=None):
     """ Main function. """
