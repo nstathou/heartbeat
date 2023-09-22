40036c19a15a29a67343fb12f31537d888787ae7
nstathou <nstathou@gmail.com>
2023-09-22 16:04:42 +0200

    agent listener accept terminal arguments

--- heartbeat/agent_listener.py
+++ heartbeat/agent_listener.py
@@ -1,68 +1,71 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
-
+import argparse
 
 class AgentListener(Node):
     """ This is the agent listener class. It is responsible for listening
         messages from the base station. """
 
-    def __init__(self):
+    def __init__(self, agent_name):
         """ Initialize the AgentListener class. """
-        super(AgentListener, self).__init__('agent_listener')
-        ## Create a subscriber.
+        super(AgentListener, self).__init__(agent_name + '_listener')
+        self.agent_name = agent_name
+        # Create a subscriber.
         self.subscription = self.create_subscription(
             String,
             'base_station/heartbeat',
             self.listener_callback,
             10)
-        self.subscription  # prevent unused variable warning
-        ## Create a publisher.
-        self.publisher = self.create_publisher(String, 'agent/status', 10)
-        ## Create a timer.
+        # Prevent unused variable warning
+        self.subscription  
+        # Create a publisher.
+        self.publisher = self.create_publisher(String, agent_name + '/status', 10)
+        # Create a timer.
         timer_period = 1.0  # seconds
         self.timer = self.create_timer(timer_period, self.status_identifier)
-        ## Create a message.
+        # Create a message.
         self.msg = ''
-        
+
     def listener_callback(self, msg):
         """ Listener callback function. """
-        self.get_logger().info('I heard: "%s"' % msg.data)
-        ## Publish a message to the agent/status topic.
+        self.get_logger().info(self.agent_name + ' heard: "%s"' % msg.data)
         self.msg = msg.data
 
     def status_identifier(self):
-        """ Status identifier function. Check is the agent is connected
-            to the base station. If yes then publish a message to the
+        """ Status identifier function. Check if the agent is connected
+            to the base station. If yes, then publish a message to the
             agent/status topic. """
-        ## Check if the agent is connected to the base station.
         if len(self.msg) == 0:
             status = 'disconnected'
-            ## Trigger agent state change.
-            self.get_logger().info('Agent state changed to: "%s"' % "returning to base station")
+            # Example of changing state depending on the status
+            self.get_logger().info(self.agent_name + ' state changed to: "returning to base station"')
         else:
             status = 'connected'
-            ## Trigger agent state change.
-            self.get_logger().info('Agent state changed to: "%s"' % "exploring")
-        ## Publish a message to the agent/status topic.
+            # Example of changing state depending on the status
+            self.get_logger().info(self.agent_name + ' state changed to: "exploring"')
+        # Publish a message to the agent/status topic.
         msg = String()
         msg.data = status
         self.publisher.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        ## Reset the message.
+        self.get_logger().info(self.agent_name + ' publishing: "%s"' % msg.data)
+        # Reset the message.
         self.msg = ''
 
 
 def main(args=None):
     """ Main function. """
     rclpy.init(args=args)
-    ## Create an AgentListener object.
-    agent_listener = AgentListener()
-    ## Spin the AgentListener object.
+    # Create an AgentListener object with the provided agent name.
+    parser = argparse.ArgumentParser(description='Agent Listener')
+    parser.add_argument('agent_name', type=str, help='Name of the agent')
+    args = parser.parse_args()
+    # Create an AgentListener object with the provided agent name.
+    agent_listener = AgentListener(args.agent_name)
+    # Keep the node alive
     rclpy.spin(agent_listener)
-    ## Destroy the AgentListener object.
+    # Destroy the node explicitly
     agent_listener.destroy_node()
-    ## Shutdown the ROS2 client.
     rclpy.shutdown()
 
 
